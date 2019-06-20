2ad4c33ec4c5a51d58445a511e3cd6e19e0268b6
Grace Lee <igracelee1@gmail.com>
2019-06-19 23:32:01 -0400

    Unsplash image API

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-      console.log(term);
+    axios.get("https://api.usplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID ec7eadecce5e7879854f9a687b0fdea87790c4766acee6c6d257b5da82c06b08"
+      }
+    });
   }
 
   render() {
