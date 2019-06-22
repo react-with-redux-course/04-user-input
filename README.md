fd4f651f8c70ff8b39910d1de6d565c7499771ae
Grace Lee <igracelee1@gmail.com>
2019-06-21 23:17:07 -0400

    react refs

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 Event handlers, "this" keyword in class componenets, and communicating child to parent components via callbacks
 
 # Making API Requests with React
-Fetching data and handling requests
+Fetching data and handling requests. Axios, Async Await, and binding callbacks
+
+# Building Lists of Records
+Rendering lists with map statement and purpose of keys in lists
+
+# Using Ref's for DOM Access
+React Ref's provides access to single DOM element
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { spans: 0 };
+        /*
+        Place refs in constructor
+        and assign to instance variable
+        */
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        /*
+            "current" is direct reference to img tag
+            "load" event will be emitted and execute callback setSpans()
+        */
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 10);
+        this.setState({ spans })
+    }
+
     render() {
         const { description, urls} = this.props.image;
         return (
-            <div>
-                <img alt={description}
+            <div style={{ gridRowEnd: `span ${this.state.spans}`}}>
+                <img
+                ref={this.imageRef}
+                alt={description}
                 src={urls.regular}
                 />
             </div>
